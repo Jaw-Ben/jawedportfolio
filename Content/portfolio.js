d3e402e72a7b2b5e352c7e330e7d0d55ffbbfc76
Jaw-Ben <jawed.bensaih@gmail.com>
2025-03-05 22:40:02 +0100

    Initial commit

---
+++ Content/portfolio.js
@@ -0,0 +1,12 @@
+/*let btn_presentation = document.getElementById("btn_presentation");
+let btn_hobbies = document.getElementById("btn_hobbies");
+let presentation = document.getElementById("presentation");
+let hobbies = document.getElementById("hobbies");
+
+
+function cacher(text, text_cache){
+    text.style.display = "block";
+    text_cache.style.display = "none";
+}
+btn_presentation.addEventListener("click", cacher(presentation, hobbies));
+btn_presentation.addEventListener("click", cacher(hobbies, presentation));*/
